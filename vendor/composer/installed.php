0f5fa190d2a29fa880a9b862b71f5bb9b9635707
Shau00n <86155250+Shau00n@users.noreply.github.com>
2022-08-06 14:46:19 +0900

    composerにようライブラリ追加

---
+++ vendor/composer/installed.php
@@ -0,0 +1,32 @@
+<?php return array(
+    'root' => array(
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'reference' => 'f2ce0cba2878771b34de841a666b42bc44307955',
+        'name' => '__root__',
+        'dev' => true,
+    ),
+    'versions' => array(
+        '__root__' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'reference' => 'f2ce0cba2878771b34de841a666b42bc44307955',
+            'dev_requirement' => false,
+        ),
+        'jwilsson/spotify-web-api-php' => array(
+            'pretty_version' => '5.2.0',
+            'version' => '5.2.0.0',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../jwilsson/spotify-web-api-php',
+            'aliases' => array(),
+            'reference' => '0d6dc349669c3cf50cf39fe3c226ca438eec0489',
+            'dev_requirement' => false,
+        ),
+    ),
+);
