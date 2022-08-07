1d3ad3e5cbf584426983f18e4c1237ec544b86e1
Kaketo Shimizu <ipkake0527@yahoo.co.jp>
2022-08-07 10:43:45 +0900

    [add] add track mock

---
+++ playlist-generator/src/app/mock.ts
@@ -0,0 +1,10 @@
+import { TrackResponse } from './interface';
+
+const Responce: TrackResponse = {
+  items: [
+    { track: { id: '2uMNMcjjUz8oNIxjIu20qE', name: 'Habit' } },
+    { track: { id: '60ReNXcPcsVFd7nUuQw5HZ', name: 'ミックスナッツ' } },
+    { track: { id: '4IfrM44LofE9bSs6TDZS49', name: 'W / X / Y' } },
+    { track: { id: '1Vqs0lXeJafYZz2sxnfB3V', name: 'シンデレラボーイ' } },
+  ],
+};
